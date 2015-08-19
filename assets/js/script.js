87c757f4fe7118889431636eafcd708dd62d29ef
Jean-François Fourmond <jffourmond@gmail.com>
2015-08-19 14:48:47 +0000

    Ajout widget HopWork

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,4 +41,15 @@ $(function () {
             waitCounter: '<span>Wait...</span>'
         });
     });
+  
+    (function(d,id) {
+       if (d.getElementById(id)) return;
+       var s = d.createElement('script');
+       var c = d.getElementsByTagName('script')[0];
+       s.type = 'text/javascript';
+       s.async = true;
+       s.src = 'https://widgets.hopwork.com/1.0.0/js/sdk.wgt.min.js';
+       c.parentNode.insertBefore(s, c);
+    })(document,'hopwork-sdkjs-wgt');
+  
 });
